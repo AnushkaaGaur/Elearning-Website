f8c0d3f21402906e287baf296b93183128646c99
Anushka <anushkagaur85@gmail.com>
2023-04-21 19:00:23 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="css/all.min.css">
+
+     <!--Google Font-->
+     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+     <!--Custom CSS-->
+     <link rel="stylesheet" href="css/style.css">
+    
+<title>EduTech</title>
+</head>
+<body>
+<!-- Start Navigation -->
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark px-5 fixed-top">
+  <a class="navbar-brand" href="index.php">EduTech</a>
+  <span class="navbar-text">Learn and Implement</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-Label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <ul class="navbar-nav custom-nav px-5">
+        <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Courses</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Payment Status</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">My Profile</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Logout</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Login</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Signup</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Feedback</a></li>
+        <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Contact Us</a></li>
+</ul>
+    </div>
+</nav>
+<!-- End Navigation -->
+
+<!-- Start Video background -->
+<div class="container-fluid remove-vid-marg">
+  <div class="vid-parent">
+  <video playsinline autoplay muted loop>
+      <source src="video/banvid.mp4">
+    </video>
+    <div class="vid-overlay"></div>
+  </div>
+  <div class="vid-content">
+    <h1 class="my-content">Welcome to EduTech</h1>
+    <small class="my-content">Learn and Implement</small> <br>
+    <a href="#"class="btn btn-danger">Start Now</a>
+  </div>
+</div>
+<!-- End Video background -->
+
+<!--Start Text Banner-->
+<div class="container-fluid bg-danger txt-banner">
+  <div class="row bottom-banner">
+    <div class="col-sm">
+      <h5><i class="fas fa-book-open mr-3"></i> 30+ Online Courses</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-users mr-3"></i> Experienced Faculties</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-keyboard mr-3"></i> Lifetime Access</h5>
+    </div>
+    <div class="col-sm">
+      <h5><i class="fas fa-inr mr-3"></i> Money Back Guarantee</h5>
+    </div>
+  </div>
+
+
+<!--End Text Banner-->
+
+
+    
+
+
+
+
+ <script src="js/jquery.min.js"></script>   
+ <script src="js/popper.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+
+ <script src="js/all.min.js"></script>
+</body>
+</html>
